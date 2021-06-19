5dfff4a4c01d74a37c2053b2747a3a329ff6856c
Mahesh <Mahesh@MaheshBabu.attlocal.net>
2021-06-19 15:35:25 -0500

    Added class

---
+++ TestProject/src/Scripts/Test_01.java
@@ -0,0 +1,10 @@
+package Scripts;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Class to push code to git");
+	}
+
+}
